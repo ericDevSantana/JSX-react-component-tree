43f8ea9ebfa28e09c309980a60382e21824178ad
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-08 14:41:14 -0700

    Input Area component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(index) {
@@ -31,10 +25,7 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
+        <InputArea onAdd={addItem} />
       </div>
       <div>
         <ul>
